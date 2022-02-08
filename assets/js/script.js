1055a9de0723a081b4975800996e607cf32d3cf2
AndrosDe <andreas_beier1981@yahoo.de>
2022-02-08 08:44:12 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+// Score
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+// Displaying the Questions
+
+function displayAdditionQuestion () {
+
+}
+
+function displaySubstractQuestion () {
+
+}
+
+function displayMultiplyQuestion () {
+
+}
+
+function displayDivisionQuestion () {  //Challange
+
+} 

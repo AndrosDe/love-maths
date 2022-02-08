6b1258e36f9bf5587155ebfb80ccb6479a83a8d5
AndrosDe <andreas_beier1981@yahoo.de>
2022-02-08 09:22:19 +0000

    Add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+// Wait until the DOM to finish loading before running the game
+// Get the button elements and event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {  // Once the DOM has finish loading this function will run
+  let buttons = document.getElementsByTagName("button");  // This will get all buttons from the page (5 in total) and put tem in an array
+  
+  // Iterating through the array can be done with: for (let i = 0; i < buttons.length; i++)
+  // Or with a newer version like this:
+  for (let button of buttons){
+    button.addEventListener("click", function(){  // Once a button has been clicked this function will run
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!")  //If the submit button was clicked, the alert will pop up
+      } else {
+          let gameType = this.getAttribute("data-type");
+          alert(`You clicked ${gameType}`); // If another button was pressed this alert will pop up to tell what button was pressed (at the beginning of writing the code to check if it is working)
+      }
+    })
+  }  
+
+})
+
 function runGame() {
 
 }

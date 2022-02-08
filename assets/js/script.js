54020f40ef9ad67139eda7e36124edaab11410d1
AndrosDe <andreas_beier1981@yahoo.de>
2022-02-08 10:35:49 +0000

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,11 @@ document.addEventListener("DOMContentLoaded", function() {  // Once the DOM has 
   for (let button of buttons){
     button.addEventListener("click", function(){  // Once a button has been clicked this function will run
       if (this.getAttribute("data-type") === "submit") {
-        alert("You clicked Submit!")  //If the submit button was clicked, the alert will pop up
+        //XXXX alert("You clicked Submit!")  //If the submit button was clicked, the alert will pop up (at the beginning of writing the code to check if it is working)
+        checkAnswer(); //actual function we want to have
       } else {
           let gameType = this.getAttribute("data-type");
-          // alert(`You clicked ${gameType}`); // If another button was pressed this alert will pop up to tell what button was pressed (at the beginning of writing the code to check if it is working)
+          //XXXX alert(`You clicked ${gameType}`); // If another button was pressed this alert will pop up to tell what button was pressed (at the beginning of writing the code to check if it is working)
           runGame(gameType); //actual function we want to have
       }
     })
@@ -34,18 +35,45 @@ function runGame(gameType) {
   if(gameType === "addition"){
     displayAdditionQuestion(num1, num2);
   } else {
-    alert(`Unknown game type: ${gameType}`); // If a different button than addition was clicked then display this alert (at the beginning of writing the code to check if it is working)
-    throw `Unknown game type: ${gameType}. Aborting`; // to stop the game from running
+      alert(`Unknown game type: ${gameType}`); // If a different button than addition was clicked then display this alert (at the beginning of writing the code to check if it is working)
+      throw `Unknown game type: ${gameType}. Aborting`; // to stop the game from running
   }
 
 }
 
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
+  let userAnswer = parseInt(document.getElementById("answer-box").value);
+  let calculatedAnswer = calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculatedAnswer[0];
 
+  if (isCorrect) {
+    alert("Hey! You got it right! :D");
+  } else {
+      alert(`Awwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+  }
+
+  runGame(calculatedAnswer[1]);
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+  let operand1 = parseInt(document.getElementById('operand1').innerText); //using parseInt to get a number and not a string (since we can't  to mathematical operations on a string)
+  let operand2 = parseInt(document.getElementById('operand2').innerText);
+  let operator = document.getElementById("operator").innerText;
 
+  if (operator === "+") {
+      return [operand1 + operand2, "addition"];
+  } else {
+      alert(`Unimplemented operator: ${operator}`); // If a different operator than addition was used then display this alert (at the beginning of writing the code to check if it is working)
+      throw `Unimplemented operator: ${operator}. Aborting`;
+  }
 }
 
 // Score
@@ -60,8 +88,7 @@ function incrementWrongAnswer() {
 
 // Displaying the Questions
 
-function displayAdditionQuestion (operand1, operand2) {
-    
+function displayAdditionQuestion (operand1, operand2) { 
   document.getElementById('operand1').textContent = operand1;
   document.getElementById('operand2').textContent = operand2;
   document.getElementById('operator').textContent = "+";

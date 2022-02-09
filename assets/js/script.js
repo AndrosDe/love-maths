c8e71ddab195902323be96329d014bf61541a0ad
AndrosDe <andreas_beier1981@yahoo.de>
2022-02-09 10:10:01 +0000

    Add multiply function to the game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Wait until the DOM to finish loading before running the game
 // Get the button elements and event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {  // Once the DOM has finish loading this function will run
-  let buttons = document.getElementsByTagName("button");  // This will get all buttons from the page (5 in total) and put tem in an array
+document.addEventListener ("DOMContentLoaded", function() {  // Once the DOM has finish loading this function will run
+  let buttons = document.getElementsByTagName ("button");  // This will get all buttons from the page (5 in total) and put tem in an array
   
   // Iterating through the array can be done with: for (let i = 0; i < buttons.length; i++)
   // Or with a newer version like this:
@@ -19,21 +19,23 @@ document.addEventListener("DOMContentLoaded", function() {  // Once the DOM has 
     })
   }
   
-  runGame("addition");
+  runGame ("addition");
 })
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(gameType) {
+function runGame (gameType) {
   // Creates two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() *25) + 1;
   let num2 = Math.floor(Math.random() *25) + 1;
 
   // Checking which game type we are running
-  if(gameType === "addition"){
+  if (gameType === "addition"){
     displayAdditionQuestion(num1, num2);
+  } else if (gameType === "multiply") {
+      displayMultiplyQuestion(num1, num2);
   } else {
       alert(`Unknown game type: ${gameType}`); // If a different button than addition was clicked then display this alert (at the beginning of writing the code to check if it is working)
       throw `Unknown game type: ${gameType}. Aborting`; // to stop the game from running
@@ -58,7 +60,7 @@ function checkAnswer() {
       incrementWrongAnswer();
   }
 
-  runGame(calculatedAnswer[1]);
+  runGame (calculatedAnswer[1]);
 }
 
 /**
@@ -72,6 +74,8 @@ function calculateCorrectAnswer() {
 
   if (operator === "+") {
       return [operand1 + operand2, "addition"];
+  } else if (operator === "x"){
+      return [operand1 * operand2, "multiply"];
   } else {
       alert(`Unimplemented operator: ${operator}`); // If a different operator than addition was used then display this alert (at the beginning of writing the code to check if it is working)
       throw `Unimplemented operator: ${operator}. Aborting`;
@@ -107,8 +111,10 @@ function displaySubstractQuestion () {
 
 }
 
-function displayMultiplyQuestion () {
-
+function displayMultiplyQuestion (operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "x";
 }
 
 function displayDivisionQuestion () {  //Challange
